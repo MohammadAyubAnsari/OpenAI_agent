eb7732878a87917db2e4e0c1bf02806738bbd3f1
Ayub <mdayubansari2014@gmail.com>
2026-06-03 14:10:43 +0530

    custom prompt

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 import "dotenv/config";
 import { Agent, run } from "@openai/agents";
 
+const location = "US";
+
 const helloAgent = new Agent({
   name: "Hello Agent",
-  instructions: "You are an agent that always says hello world with users name",
+  instructions: function () {
+    if (location === "India") {
+      return "Always say Namaste and then You are an agent that always says hello world with users name";
+    } else {
+      return "That just talk to the user ";
+    }
+  },
+  model: "gpt-4o-mini",
 });
 
-run(helloAgent, "Hey there, my name is Bob").then((result) => {
+run(helloAgent, "Hey there, My name is Bob").then((result) => {
   console.log(result.finalOutput);
 });
