2a81710b09fef82569410c3f0dc76c87dcc6adcb
Ayub <mdayubansari2014@gmail.com>
2026-06-07 09:00:03 +0530

    Refund agent

--- agent_manager.js
+++ agent_manager.js
@@ -1,6 +1,7 @@
 import "dotenv/config";
 import { Agent, run, tool } from "@openai/agents";
 import { z } from "zod";
+import fs from "node:fs/promises";
 
 const fetchAvailablePlans = tool({
   name: "fetch_available_plans",
@@ -15,11 +16,40 @@ const fetchAvailablePlans = tool({
   },
 });
 
+const processRefund = tool({
+  name: "process_refund",
+  description: "This tool processes the refund for a customer",
+  parameters: z.object({
+    customerId: z.string().describe("id of the customer"),
+    reason: z.string().describe("reason for refund"),
+  }),
+  execute: async function ({ customerId, reason }) {
+    await fs.appendFile(
+      "./refunds.txt",
+      `Refund for Customer having ID ${customerId} for ${reason}\n`,
+      "utf-8",
+    );
+    return { refundIssued: true };
+  },
+});
+
+const refundAgent = new Agent({
+  name: "Refund Agent",
+  instructions: "You are an expert in issuing refunds .",
+  tools: [processRefund],
+});
+
 const sales_agent = new Agent({
   name: "sales_agent",
   instructions:
     "You are an expert sales agent for an internet broadband company.Talk to the user and help them with what they need.",
-  tools: [fetchAvailablePlans],
+  tools: [
+    fetchAvailablePlans,
+    refundAgent.asTool({
+      toolName: "refund_expert",
+      toolDescription: "Handles refund questions and requests.",
+    }),
+  ],
 });
 
 async function runAgent(query = "") {
@@ -27,4 +57,6 @@ async function runAgent(query = "") {
   console.log(result.finalOutput);
 }
 
-runAgent(`Hey there, I want to know the available plans`);
+runAgent(
+  `I had a plan 399. I need only refund right now. My customer id is 123 because I am shifting to a new place`,
+);
